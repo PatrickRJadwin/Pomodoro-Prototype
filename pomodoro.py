132c3c7322c68ef894ea0f13bfc883cf322d1222
Patrick Jadwin <patrick_jadwin@discovery.com>
2019-04-03 10:46:18 -0400

    Adjust output to display accurate time

--- pomodoro.py
+++ pomodoro.py
@@ -5,11 +5,11 @@ import sys
 import tkinter as tk
 from tkinter import messagebox
 
-minute = 60
+minute = 1 
 
 
 def breakpnt(breaktime):
-    messagebox.showwarning("Time is up!", "Please take a 5 minute break starting now")
+    messagebox.showwarning("Time is up!", "Please take a " + str(breaktime) + " minute break starting now")
     root.update
     x = breaktime
     for i in range(breaktime):
@@ -37,7 +37,7 @@ print("\n\n Starting.... \n\n")
 while True:
     if mins <= count:
         print(">>>>>>>>>>>> " + str(mins))
-        time.sleep(60) #60 = 1 min
+        time.sleep(minute) #60 = 1 min
         mins += 1
     elif breakcnt < 3:
         breakpnt(5) 
